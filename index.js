86f504fd37a4022d2232c76091062c04c1ac2461
Vladoxx <wladox@outlook.com>
2020-09-20 23:05:37 -0300

    Commit Dois

--- index.js
+++ index.js
@@ -10,38 +10,48 @@ const tasks = Task(sequelize, DataTypes)
 app.use(express.json())
 
 // List tasks
-app.get('/tasks', (req, res) => {
-  res.json({ action: 'Listing tasks' })
-})
+app.get('/tasks', async(req, res) => {
+  const storage = await tasks.findAll();
+  res.json({ storage });
+});
 
 // Create task
-app.post('/tasks', (req, res) => {
-  const body = req.body
+app.post('/tasks', async(req, res) => {
+  const body = req.body;
 
-  res.json(body)
-})
+  await tasks.create({
+    description: body.description,
+    createdAt: body.createdAt
+  });
+
+  res.json({ result: "New task", taskId: taskId });
+});
 
 // Show task
-app.get('/tasks/:id', (req, res) => {
-  const taskId = req.params.id
+app.get('/tasks/:id', async(req, res) => {
+  const taskId = req.params.id;
+  const task = await tasks.findByPk(id);
 
-  res.send({ action: 'Showing task', taskId: taskId })
-})
+  res.send({ action: 'Showing task', taskId: taskId });
+});
 
 // Update task
-app.put('/tasks/:id', (req, res) => {
+app.put('/tasks/:id', async(req, res) => {
+  const body = req.body;
   const taskId = req.params.id
+  const task = await tasks.findByPk(id);
 
   res.send({ action: 'Updating task', taskId: taskId })
-})
+});
 
 // Delete task
-app.delete('/tasks/:id', (req, res) => {
+app.delete('/tasks/:id', async(req, res) => {
   const taskId = req.params.id
+  await tasks.destroy({ where: {id: 2 } });
 
-  res.send({ action: 'Deleting task', taskId: taskId })
-})
+  res.send({ action: 'Deleting task', taskId: taskId });
+});
 
 app.listen(3000, () => {
-  console.log('Iniciando o ExpressJS na porta 3000')
-})
+  console.log('Server running')
+});
